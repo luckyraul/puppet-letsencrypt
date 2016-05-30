2667dfc2aed54dfd27fcb972bf0f2a3715d7b92a
Nikita Tarasov <nikita@mygento.ru>
2016-05-30 16:23:41 +0300

    upd

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -2,10 +2,10 @@
 class letsencrypt::cron {
 
   cron { 'letsencrypt-renew':
-    command => 'letsencrypt renew',
-    user    => 'root',
-    minute  => '11',
-    hour    => '1',
-    date    => '1',
+    command  => 'letsencrypt renew',
+    user     => 'root',
+    minute   => '11',
+    hour     => '1',
+    monthday => '1',
   }
 }
